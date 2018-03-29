fa136f984197da8550840ea20f8c25f93c8e0681
saleemchoudary <saleemtpcinfodeveloper@gmail.com>
2018-03-29 13:03:05 +0530

    Initial commit

---
+++ app/src/main/java/com/techkshetrainfo/myapplication/Constants.java
@@ -0,0 +1,16 @@
+package com.techkshetrainfo.myapplication;
+
+/**
+ * Created by tps on 3/29/2018.
+ */
+
+public class Constants {
+
+    public static final String M_ID = "Techks85261789860550"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String CHANNEL_ID = "WAP"; //Paytm Channel Id, got it in paytm credentials
+    public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
+
+    public static final String WEBSITE = "APP_STAGING";
+    public static final String CALLBACK_URL = "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp";
+
+}
